b888a928c971f9bf996e2e2e61ae97c74c8c4138
GiovanniGiannitto <97680539+GiovanniGiannitto@users.noreply.github.com>
2022-03-28 19:25:29 +0200

    Add files via upload

---
+++ es-8/exercise-5.js
@@ -0,0 +1,24 @@
+function calculateAverageAge(persons) {
+  let sumAge = 0;
+  for(let i = 0; i < persons.length; i++) {
+    sumAge += persons[i].age;
+  }
+  return sumAge / persons.length;
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const averageAge = calculateAverageAge(persons);
+console.log(persons);
+console.log(averageAge);
