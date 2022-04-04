ea08e860885a1a94b76f49af0eb807d4e09938c3
GiovanniGiannitto <97680539+GiovanniGiannitto@users.noreply.github.com>
2022-04-04 16:49:13 +0200

    Update exercise-4.js

--- es-52/exercise-4.js
+++ es-52/exercise-4.js
@@ -3,14 +3,13 @@ const person = {
   firstName: 'Mario',
   lastName: 'Rossi',
   age: 25,
-  replacer(key, value) {
-    if(typeof value === "string") {
-      return undefined;
-    }
-    return value;
-  }
 };
 
-const json = JSON.stringify(person, ["id", "age"]);
+const json = JSON.stringify(person, (key, value) => {
+  if (typeof value === "string") {
+    return undefined;
+  }
+  return value;
+});
 
 console.log(json); // Should return: { id: 1, age: 25 }
