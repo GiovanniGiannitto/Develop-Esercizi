789af8c2e5e155f284eafe784e4d762bdbba2d97
GiovanniGiannitto <97680539+GiovanniGiannitto@users.noreply.github.com>
2022-04-13 19:30:17 +0200

    Add files via upload

---
+++ es-60/exercise-4.js
@@ -0,0 +1,68 @@
+const persons = [
+  {
+    id: 1,
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+  },
+  {
+    id: 2,
+    firstName: 'Maria',
+    lastName: 'Verdi',
+    age: 32
+  },
+  {
+    id: 3,
+    firstName: 'Giovanni',
+    lastName: 'Rossi',
+    age: 35
+  }
+];
+
+const jobs = [
+  {
+    id: 1,
+    jobTitle: 'CEO'
+  },
+  {
+    id: 2,
+    jobTitle: 'Project Manager'
+  },
+  {
+    id: 3,
+    jobTitle: 'Developer'
+  }
+];
+
+// core here
+
+function fetchPersonById(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const person = persons.find((item) => item.id === id);
+      if (person) {
+        resolve(person);
+      } else {
+        reject(new Error("id not found"));
+      }
+    }, 1000);
+  });
+}
+
+function fetchJobById(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const job = jobs.find((item) => item.id === id);
+      if (job) {
+        resolve(job);
+      } else reject(new Error("job not found"));
+    }, 1000);
+  });
+}
+
+Promise.all([fetchPersonById(4), fetchJobById(3)]).then((value) =>console.log(value));
+
+
+
+
+
